d0d407fae830dd7752738bc2947bfd515c430828
Carlos <carloxdev@gmail.com>
2017-03-23 09:24:29 -0600

    Avance de serializacion de Cabecera y Lineas

--- finanzas/urls.py
+++ finanzas/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
 from .views import ViaticoNuevo
-from .views import ViaticoLinea
+from .views import ViaticoLineaLista
 
 urlpatterns = [
     url(r'^viatico/nuevo/$', ViaticoNuevo.as_view(), name="viatico_nuevo"),
-    url(r'^viatico/solicitudes/$', ViaticoLinea.as_view(), name="viatico_solicitudes"),
+    url(r'^viatico/solicitudes/$', ViaticoLineaLista.as_view(), name="viatico_solicitudes"),
 ]
--- finanzas/views.py
+++ finanzas/views.py
@@ -1,7 +1,24 @@
+# Librerias de Django
 from django.views.generic import CreateView
 from django.views.generic import ListView
-from .forms import ViaticoCabeceraForm, ViaticoLineaForm
-from .models import ViaticoCabecera, ViaticoLinea
+
+# Librerias Python
+
+# Librerias de Terceros:
+# API Rest:
+from rest_framework import viewsets
+
+# Modelos:
+from .models import ViaticoCabecera
+from .models import ViaticoLinea
+
+# Formularios
+from .forms import ViaticoCabeceraForm
+from .forms import ViaticoLineaForm
+
+# Serializadores:
+from .serializers import ViaticoCabeceraSerializer
+from .serializers import ViaticoLineaSerializer
 
 
 class ViaticoNuevo(CreateView):
@@ -20,7 +37,21 @@ class ViaticoNuevo(CreateView):
         return context
 
 
-class ViaticoLinea(ListView):
-    model = ViaticoCabecera
+class ViaticoLineaLista(ListView):
+    model = ViaticoLinea
     template_name = 'viatico/viatico_solicitudes.html'
     context_object_name = 'viaticos_solicitudes'
+
+
+class ViaticoCabeceraAPI(viewsets.ModelViewSet):
+    queryset = ViaticoCabecera.objects.all()
+    serializer_class = ViaticoCabeceraSerializer
+    # pagination_class = GenericPagination
+
+    # filter_backends = (DjangoFilterBackend,)
+    # filter_class = EquipoFilter
+
+
+class ViaticoLineaAPI(viewsets.ModelViewSet):
+    queryset = ViaticoLinea.objects.all()
+    serializer_class = ViaticoLineaSerializer
