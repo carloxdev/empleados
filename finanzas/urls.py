2df765e09bc87ee0817746b041447174e05beeeb
Carlos <carloxdev@gmail.com>
2017-04-18 11:31:41 -0500

    Se corrigió la url de viaticos

--- finanzas/urls.py
+++ finanzas/urls.py
@@ -6,7 +6,7 @@ from .views import ViaticoEditar
 
 
 urlpatterns = [
-    url(r'^viaticos$', ViaticoLista.as_view(), name="viatico_lista"),
+    url(r'^viaticos/$', ViaticoLista.as_view(), name="viatico_lista"),
     url(r'^viaticos/nuevo/$', ViaticoNuevo.as_view(), name="viatico_nuevo"),
     url(r'^viaticos/editar/(?P<pk>\d+)/$', ViaticoEditar.as_view(), name="viatico_editar"),
 ]
