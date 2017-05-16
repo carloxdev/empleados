491678413c7177419e21775642980479165a84c3
Carlos <carloxdev@gmail.com>
2017-05-16 12:08:16 -0500

    Avance de Viáticos

--- home/static/master.js
+++ home/static/master.js
@@ -2,6 +2,8 @@
             GLOBAL VARIABLES
 \*-----------------------------------------------*/
 
+
+var nova_sitio = null
 var galletita = ""
 
 
@@ -11,5 +13,31 @@ var galletita = ""
 
 $(document).ready(function () {
     
-    galletita = $.cookie('csrftoken')
+    nova_sitio = new NovaSitio()
 })
+
+
+
+
+/*-----------------------------------------------*\
+            OBJETO: NovaSitio
+\*-----------------------------------------------*/
+
+function NovaSitio() {
+
+    this.galletita = $.cookie('csrftoken')
+    this.$submodulo = $("#submodulo")
+
+    this.set_ActivePage()
+}
+NovaSitio.prototype.set_ActivePage = function () {
+    if (this.$submodulo.text() == "Solicitudes de Viaticos") 
+    {
+        this.activar_Opcion("solicitudes")
+    }
+}
+NovaSitio.prototype.activar_Opcion = function (_option) {
+
+    var $opcion = $("#" + _option)
+    $opcion.addClass("active")
+}
