1acfa3a6d5aff74f89e862879c3a8da7e2cecd74
Janet <janet.castro@nuvoil.com>
2017-06-30 10:58:51 -0500

    Seguridad laboral - sgi

--- seguridadlaboral/utilities.py
+++ seguridadlaboral/utilities.py
@@ -2,12 +2,31 @@
 # Librerias Python
 import os
 
+# para guardar el archivo por carpeta con el nombre de cada incidencia y dentro una carpeta de files
+
+# def get_FilePath_Incidencia(instance, filename):
+
+#     if (instance.incidencia_id):
+#         upload_dir = os.path.join(
+#             'incidencias', str(instance.incidencia_id), 'files')
+
+#     return os.path.join(upload_dir, filename)
+
+
+# def get_FilePath_Resolucion(instance, filename):
+
+#     if instance.resolucion_id:
+#         upload_dir = os.path.join(
+#             'incidencias', str(instance.resolucion.incidencia_id), 'files', 'resoluciones')
+
+#     return os.path.join(upload_dir, filename)
+
 
 def get_FilePath_Incidencia(instance, filename):
 
     if (instance.incidencia_id):
         upload_dir = os.path.join(
-            'incidencias', str(instance.incidencia_id), 'files')
+            'incidencias', str(instance.incidencia_id))
 
     return os.path.join(upload_dir, filename)
 
@@ -18,4 +37,4 @@ def get_FilePath_Resolucion(instance, filename):
         upload_dir = os.path.join(
             'incidencias', str(instance.resolucion.incidencia_id), 'files', 'resoluciones')
 
-    return os.path.join(upload_dir, filename)
+    return os.path.join(upload_dir, filename)    
