504ce23bae5571facdca46c603eed54f03b84df4
Nadia <nadia.cordova@nuvoil.com>
2017-09-19 17:36:13 -0500

    Aplicación editorial

--- editorial/apps.py
+++ editorial/apps.py
@@ -1,8 +1,8 @@
-# # -*- coding: utf-8 -*-
-# from __future__ import unicode_literals
-#
-# from django.apps import AppConfig
-#
-#
-# class EditorialConfig(AppConfig):
-#     name = 'editorial'
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class EditorialConfig(AppConfig):
+    name = 'editorial'
--- editorial/utilities.py
+++ editorial/utilities.py
@@ -1,13 +1,13 @@
-# # Python's Libraries
-# import os
-#
-#
-# def get_ImagePath_Post(instance, filename):
-#
-#     if (instance):
-#         upload_dir = os.path.join(
-#             'images',
-#             'post',
-#         )
-#
-#     return os.path.join(upload_dir, filename)
+# Python's Libraries
+import os
+
+
+def get_ImagePath_Post(instance, filename):
+
+    if (instance):
+        upload_dir = os.path.join(
+            'images',
+            'post',
+        )
+
+    return os.path.join(upload_dir, filename)
