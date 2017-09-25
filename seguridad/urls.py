f9985d768f1d1f17d874fc856cd698f89959f509
Carlos <carloxdev@gmail.com>
2017-09-25 09:58:42 -0500

    Se ordenaron urls en seguridad

--- seguridad/urls.py
+++ seguridad/urls.py
@@ -25,75 +25,27 @@ app_name = "seguridad"
 
 urlpatterns = [
 
-    url(
-        r'^$',
-        Login.as_view(),
-        name='login'
-    ),
-    url(
-        r'^logout/$',
-        auth_views.logout,
-        {'next_page': settings.LOGIN_URL},
-        name='logout'
-    ),
-    url(
-        r'^registro/$',
-        Registro.as_view(),
-        name="usuario_registro"
-    ),
-    url(
-        r'^registro/exito/(?P<_pk>\d+)/$',
-        RegistroExito.as_view(),
-        name="usuario_registro_exito"
-    ),
-    url(
-        r'^contrasena_reset/$',
-        ContrasenaReset.as_view(),
-        name='contrasena_reset'
-    ),
+    url(r'^$', Login.as_view(), name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': settings.LOGIN_URL}, name='logout'),
+    url(r'^registro/$', Registro.as_view(), name="usuario_registro"),
+    url(r'^registro/exito/(?P<_pk>\d+)/$', RegistroExito.as_view(), name="usuario_registro_exito"),
+    url(r'^contrasena_reset/$', ContrasenaReset.as_view(), name='contrasena_reset'),
+
     # Pantalla de restablecimiento de contrasena.
-    url(
-        r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        ContrasenaResetConfirm.as_view(),
-        name='contrasena_reset_confirm'
-    ),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        ContrasenaResetConfirm.as_view(), name='contrasena_reset_confirm'),
+
     # Mensaje 'success' de contrasena cambiada.
-    url(
-        r'^reset/done/$',
-        ContrasenaResetComplete.as_view(),
-        name='contrasena_reset_complete'
-    ),
-    url(
-        r'^usuarios/$',
-        UsuarioLista.as_view(),
-        name="usuario_lista"
-    ),
-    url(
-        r'^usuarios/nuevo/$',
-        UsuarioNuevo.as_view(),
-        name="usuario_nuevo"
-    ),
-    url(
-        r'^usuarios/(?P<_pk>\d+)/editar/$',
-        UsuarioEditar.as_view(),
-        name="usuario_editar"
-    ),
-    url(
-        r'^usuarios/(?P<_pk>\d+)/editar/contrasena/$',
-        UsuarioEditarContrasena.as_view(),
-        name="usuario_editar_contrasena"
-    ),
-    url(
-        r'^usuarios/(?P<_pk>\d+)/perfil/$',
-        UsuarioPerfil.as_view(),
-        name="usuario_perfil"
-    ),
-    url(
-        r'^usuarios/(?P<_pk>\d+)/perfil/contrasena/$',
-        UsuarioPerfilContrasena.as_view(),
-        name="usuario_perfil_contrasena"
-    ),
-    url(r'^autorizacion/$', Autorizacion.as_view(), name="autorizacion"),
+    url(r'^reset/done/$', ContrasenaResetComplete.as_view(), name='contrasena_reset_complete'),
+    url(r'^usuarios/$', UsuarioLista.as_view(), name="usuario_lista"),
+    url(r'^usuarios/nuevo/$', UsuarioNuevo.as_view(), name="usuario_nuevo"),
+    url(r'^usuarios/(?P<_pk>\d+)/editar/$', UsuarioEditar.as_view(), name="usuario_editar"),
+    url(r'^usuarios/(?P<_pk>\d+)/editar/contrasena/$', UsuarioEditarContrasena.as_view(),
+        name="usuario_editar_contrasena"),
+    url(r'^usuarios/(?P<_pk>\d+)/perfil/$', UsuarioPerfil.as_view(), name="usuario_perfil"),
+    url(r'^usuarios/(?P<_pk>\d+)/perfil/contrasena/$', UsuarioPerfilContrasena.as_view(),
+        name="usuario_perfil_contrasena"),
+    url(r'^autorizacion/$',  Autorizacion.as_view(), name="autorizacion"),
 ]
 
 if settings.DEBUG:
