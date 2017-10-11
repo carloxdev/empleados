d535ae45396359bb9babbaa5a4a2d8ea17e83435
Carlos <carloxdev@gmail.com>
2017-10-11 18:15:37 -0500

    Fix pace

--- home/static/master.js
+++ home/static/master.js
@@ -16,6 +16,9 @@ window.paceOptions = {
     // Only show the progress on regular and ajax-y page navigation,
     // not every request
     restartOnRequestAfter: false,
+    ajax: {
+      trackMethods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH']
+    }
 }
 
 $(document).ready(function () {
