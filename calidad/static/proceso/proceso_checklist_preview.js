914193d876d8564b841bc09b934c354554acf8a8
Zachariel Arias <ariaszacariaszach@gmail.com>
2017-10-23 06:10:03 -0500

    Fix al autoespaciado al generar los pdf
    Se soluciono el problema del cortado de tablas que se quedaban sobre los margenes

--- calidad/static/proceso/proceso_checklist_preview.js
+++ calidad/static/proceso/proceso_checklist_preview.js
@@ -22,11 +22,11 @@ function TarjetaResultados() {
 
    this.$texto_dinamico = $('#texto-dinamico')
    this.$contenido = $('#contenido')
-   var margen_top_pdf = 4.5
-   this.$mt = this.get_Pixeles(34+margen_top_pdf) //Definir el espacio usado como margen para top en mm
+   this.$mt = this.get_Pixeles(34) //Definir el espacio usado como margen para top en mm
    this.$mb = this.get_Pixeles(30) //Definir el espacio usado como margen para bottom en mm
+   this.$altura = 279.4 //Definir la altura del tipo de documento (279.4mm para tamaño carta)
    console.log(this.get_TextoDinamico())
-   this.set_NumeroPaginas()
+   this.set_NumeroPaginas(this.$altura)
    window.print()
 }
 TarjetaResultados.prototype.get_Pixeles = function (mm) {
@@ -37,15 +37,20 @@ TarjetaResultados.prototype.get_TextoDinamico = function () {
 
    var totalHeight = 0
    var espacio_ocupado = 0
-   var espacio_total = this.get_Pixeles(309) //Espacio total del pdf en pixeles(1167.875449). Nota: En media print, por alguna razon el alto del pdf es de 309mm para tamaño carta de hoja
+   var espacio_total = this.get_Pixeles(this.$altura) //Espacio de altura del pdf en pixeles
    var espacio_disponible = espacio_total - (this.$mt + this.$mb)
-
 
    this.$texto_dinamico.find('.nova-dinamic').each(function(index, element) {
 
       if ((espacio_ocupado + $(element).outerHeight( true )) > espacio_disponible) {
          //antes del elemento actual, inserta un salto de linea. Porque el elemento actual es el que sobresalta espacio disponible
-         $(element).before('<div class="page-break"></div><div class="nova-pdf-header-fix"></div><div class="calidad-paginado"> de </div>'); // insertar un div con el alto del footer
+         if ( $(element).hasClass('nova-be-tr') ) {
+            $(element).before('<div class="page-break"></div><div class="page-break"></div><div class="nova-pdf-header-fix"></div><div class="calidad-paginado"></div>'); // Doble break e insertar un div con el alto del footer
+         }
+         else {
+            $(element).before('<div class="page-break"></div><div class="nova-pdf-header-fix"></div><div class="calidad-paginado"></div>'); // Break e insertar un div con el alto del footer
+         }
+
          espacio_ocupado = 0  //Despues de un salto de pagina reestablece la altura del espacio ocupado
       }
 
@@ -56,12 +61,15 @@ TarjetaResultados.prototype.get_TextoDinamico = function () {
 
    return totalHeight;
 }
-TarjetaResultados.prototype.set_NumeroPaginas = function () {
+TarjetaResultados.prototype.set_NumeroPaginas = function (altura) {
 
    var total_paginas = this.$contenido.find('.calidad-paginado').size()
+   var top = 25
 
    this.$contenido.find('.calidad-paginado').each(function(index, element) {
-      var pagina = index + 1
-      $(element).html( pagina + " de " + total_paginas)
+
+
+      $(element).html( (index + 1) + " de " + total_paginas).css({'top' : top + 'mm', 'left':'70mm'});
+      top +=altura
    })
 }
